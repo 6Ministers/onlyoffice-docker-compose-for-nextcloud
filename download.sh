ed25904573ac40017d55d5672784388bca5f9d3e
Eduard <markerbiznes@gmail.com>
2023-12-24 17:42:24 +0300

    Update

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 DESTINATION=$1
 
 # clone Flectra directory
-git clone --depth=1 https://github.com/6Ministers/nextcloud-docker-compose-all-in-one-onlyoffice $DESTINATION
+git clone --depth=1 https://github.com/6Ministers/onlyoffice-for-nextcloude $DESTINATION
 rm -rf $DESTINATION/.git
 
 
