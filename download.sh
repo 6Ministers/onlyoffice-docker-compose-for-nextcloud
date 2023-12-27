fc6d5f441c8c99a2960572d571d20b4d90b7661d
6ministers <manyatovskiy@gmail.com>
2023-12-27 20:54:05 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 DESTINATION=$1
 
 # clone Flectra directory
-git clone --depth=1 https://github.com/6Ministers/onlyoffice-for-nextcloude $DESTINATION
+git clone --depth=1 https://github.com/6Ministers/onlyoffice-docker-compose-for-nextcloude $DESTINATION
 rm -rf $DESTINATION/.git
 
 
